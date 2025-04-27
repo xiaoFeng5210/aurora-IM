bbca4cc7291dff9a2b87b195518b716bcedf0df8
Eadweard <aurora.coder.zqf@gmail.com>
2025-04-27 12:59:07 +0800

    feat: server/main.go修改

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module aurora-im
 
 go 1.24.1
 
-require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 func InitLogger() {
@@ -12,4 +17,29 @@ func InitLogger() {
 
 func main() {
 	InitLogger()
+}
+
+var upgrader = websocket.Upgrader{
+	HandshakeTimeout: 1 * time.Second,
+	ReadBufferSize:   100,
+	WriteBufferSize:  100,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// 用户注册，不需要传参数，返回用户id
+func RegistUser(w http.ResponseWriter, r *http.Request) {
+	uid := time.Now().UnixMicro() //生成全局唯一的群ID（正规来讲，应该借助于mysql的自增id）
+	err := GetRabbitMQ().RegisterUser(uid, "u")
+	if err != nil {
+		log.Printf("创建用户%d失败:%s", uid, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("创建用户失败"))
+		return
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(strconv.FormatInt(uid, 10)))
+		return
+	}
 }
